5534163d81625c52af0d6071ce006b6ab2179dae
DM_churey <d.chureev2002@gmail.com>
2024-10-15 20:40:30 +0300

    feat: create new interfaces

---
+++ src/app/models/account.ts
@@ -0,0 +1,9 @@
+export interface AccountDataInterface {
+    id: number;
+    number: string;
+    name: string;
+    balance: number;
+    currency: number;
+    createdDate: string;
+    state: string;
+}
