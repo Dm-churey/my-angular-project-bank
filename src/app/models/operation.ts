b23d1c04dc7d03e943fd89cee2483f4f61124323
DM_churey <d.chureev2002@gmail.com>
2024-10-21 16:53:48 +0300

    feat(operation.ts): add new interface

---
+++ src/app/models/operation.ts
@@ -0,0 +1,39 @@
+export interface CardInfoInterface {
+    id: number;
+    name: string;
+    image: string;
+    shortDescription: string;
+}
+
+export interface StartOperationRequestInterface {
+    operationCode: string;
+}
+
+export interface OperationResponceInterface {
+    clientId: number;
+    isConfirmed: boolean;
+    isFinished: boolean;
+    name: string;
+    operationCode: string;
+    requestId: number;
+    startDate: Date;
+    stepId: number;
+    stepParams: StepParam[]  
+}
+
+export interface StepParam {
+    identifier: string;
+    value: string | null;
+    name: string;
+    requirements: Requirement;
+    type: string;
+    values: string[];
+}
+
+export interface Requirement {
+    mask: string | null;
+    max: number | null;
+    min: number | null;
+    required: boolean;
+}
+  
