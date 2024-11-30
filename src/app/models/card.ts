1ec3c7eb3de8cc23938d62c024ce1a7599482881
DM_churey <d.chureev2002@gmail.com>
2024-11-30 14:18:27 +0300

    feat(card.ts): add new methods

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,9 +1,20 @@
+import { AccountDataInterface } from "./account";
+import { ClientInterface } from "./client";
+import { CardInfoInterface } from "./operation";
+
 export interface CardDataInterface {
     id: number;
+    account: AccountDataInterface;
+    client: ClientInterface;
+    product: CardInfoInterface;
     number: string;
     month: string;
     year: string;
     cardProgram: string;
     pinRequired: boolean;
     state: string;
+}
+
+export interface PinCodeInterface {
+    pinCode: number;
 }
